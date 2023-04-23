d6b1c3d52ef77e62fd76baa43735e539dea0e274
gabrielres10 <93087966+gabrielres10@users.noreply.github.com>
2023-04-22 20:33:37 -0500

    Feat: Update views.py file

--- src/kp_app/views.py
+++ src/kp_app/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 
 # Create your views here.
+@login_required
 def home(request):
-    return render(request, "login.html", {
-        'user': "page_manager"
+    return render(request, "base.html", {
+        'user': "page_manager",
     })
+
+def signout(request):
+    logout(request)
+    return redirect('/')
+
+
+def second(request):
+    return render(request, "base.html")
