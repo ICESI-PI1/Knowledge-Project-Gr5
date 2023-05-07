24d61a39d164465e6f2b3170d011e766b60785a2
JDGarzon <93989744+JDGarzon@users.noreply.github.com>
2023-05-07 15:45:35 -0500

    test: Adding tests to new model (Company,Category,Project)

--- src/knowledge_project/apps/app_projects/tests.py
+++ src/knowledge_project/apps/app_projects/tests.py
@@ -7,16 +7,8 @@ from datetime import *
 class CompanyModelTestCase(TestCase):
     
     def setUp(self):
-        self.user = User.objects.create(
-            phone='+573001234567',
-            email='testuser@example.com',
-            birth_date=datetime.today().date() - timedelta(days=365 * 20),
-            full_name='Test User',
-            password='testpassword',
-            user_cc='1234567890'
-        )
         self.company = Company.objects.create(
-            nit='123456789',
+            nit='1234567890',
             name='Test Company',
             address='ddf',
             phone='+573001234567'
@@ -24,4 +16,122 @@ class CompanyModelTestCase(TestCase):
         
     def test_company_creation(self):
         self.assertEqual(self.company.name, 'Test Company')
-        
+        self.assertEqual(self.company.phone,'+573001234567')
+        self.assertEqual(self.company.address,'ddf')
+        self.assertEqual(self.company.nit,'1234567890')
+    
+    def test_company_str_representation(self):
+        expected_str = 'Test Company - NIT:  1234567890'
+        self.assertEqual(self.company.__str__(), expected_str)
+
+    def test_company_nit_validation(self):
+
+        # Test invalid NIT with more than 10 digits
+        with self.assertRaises(ValidationError):
+            Company.full_clean(Company.objects.create(
+            nit='12345678899',
+            name='Test Company',
+            address='ddf',
+            phone='+573001234567'
+        ))
+
+    def test_company_phone_validation(self):
+        # Test valid phone number
+        self.company.phone = "123456789"
+        self.company.save()
+        self.assertEqual(self.company.phone, "123456789")
+
+        # Test invalid phone number with less than 8 digits
+        with self.assertRaises(ValidationError):
+            Company.full_clean(Company.objects.create(
+            nit='12345678899',
+            name='Test Company',
+            address='ddf',
+            phone='1234567'
+        ))
+
+        # Test invalid phone number with more than 10 digits
+        with self.assertRaises(ValidationError):
+             Company.full_clean(Company.objects.create(
+            nit='12345678891',
+            name='Test Company',
+            address='ddf',
+            phone='1234567891011'
+        ))
+
+class CategoryModelTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+            name="Test Category",
+            photo="path/to/photo.jpg"
+        )
+
+    def test_category_str_representation(self):
+        expected_str = "1 - Name: Test Category"
+        self.assertEqual(str(self.category), expected_str)
+
+    def test_category_photo_upload(self):
+        self.assertEqual(self.category.photo, "path/to/photo.jpg")
+
+    def test_category_photo_upload_blank(self):
+        category = Category.objects.create(
+            name="Blank Photo Category",
+            photo=""
+        )
+        self.assertEqual(category.photo, "")
+
+    def test_category_photo_upload_null(self):
+        category = Category.objects.create(
+            name="Null Photo Category",
+            photo=None
+        )
+        self.assertEqual(category.photo, None)
+
+
+class ProjectModelTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+            name="Test Category",
+            photo="path/to/photo.jpg"
+        )
+        self.company = Company.objects.create(
+            nit="1234567890",
+            phone="123456789",
+            address="Test Address",
+            name="Test Company"
+        )
+        self.project = Project.objects.create(
+            title="Test Project",
+            objective="Test Objective",
+            results="Test Results",
+            reach="Test Reach",
+            company_nit=self.company,
+            category=self.category
+        )
+
+    def test_project_str_representation(self):
+        expected_str = f"{self.project.id_project} - Objective: Test Objective, Company: Test Company"
+        self.assertEqual(str(self.project), expected_str)
+
+    def test_project_state_default(self):
+        self.assertEqual(self.project.state, "Propuesta")
+
+    def test_project_state_choices(self):
+        with self.assertRaises(ValidationError):
+            Project.full_clean(
+                Project.objects.create(
+                    title="Test Project",
+                    objective="Test Objective",
+                    results="Test Results",
+                    reach="Test Reach",
+                    company_nit=self.company,
+                    category=self.category,
+                    state="otro"
+                )
+            )
+        choices = (
+            ("Propuesta", "Propuesta"),
+            ("En convocatoria", "En convocatoria"),
+            ("En factory", "En factory")
+        )
+        self.assertEqual(self.project.STATE_CHOICES, choices)
