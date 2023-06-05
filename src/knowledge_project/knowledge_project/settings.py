f6e7138563ef9e3e695a17294a1d0826d063fc7a
Geovanny-Quintero-Velez <geovannyalexander07@gmail.com>
2023-06-05 18:20:12 -0500

    "Settings: add DB configurations"

--- src/knowledge_project/knowledge_project/settings.py
+++ src/knowledge_project/knowledge_project/settings.py
@@ -86,10 +86,7 @@ WSGI_APPLICATION = "knowledge_project.wsgi.application"
 
 DATABASES = {
 
-   "default": {
-      "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "Knowledge_Projects.db",
-   },
+   "default": dj_database_url.config(default="sqlite:///Knowledge_Projects.db", conn_max_age=600),
     "test": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "Knowledge_Projects.db",
@@ -137,7 +134,7 @@ STATIC_URL = "static/"
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
