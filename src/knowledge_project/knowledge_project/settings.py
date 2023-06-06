12d5e6354a8c3f81f9a056832fc72aade2dbc575
Geovanny-Quintero-Velez <geovannyalexander07@gmail.com>
2023-06-05 19:51:35 -0500

    "Settings: Fix deploy configs"

--- src/knowledge_project/knowledge_project/settings.py
+++ src/knowledge_project/knowledge_project/settings.py
@@ -132,14 +132,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    ) 
-else:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_URL = "/users/login/"
 
